47dbe3d2129107da8384e947082bb4510af1a3b1
Santhoshi0708 <136299190+Santhoshi0708@users.noreply.github.com>
2024-04-09 20:27:50 +0530

    Add files via upload

---
+++ WeekDay.py
@@ -0,0 +1,16 @@
+import streamlit as srt #A library to easify the user without requiring front end knowledge
+import datetime # A builtin module to use date related builtin functions
+import calendar # A builtin module to use calendar related functions
+
+#To represent the tile to our web page
+str.title("WeekDay Using streamlit")
+Input_date=srt.date_input("Enter DateInput")
+#Days of a week
+Weekdays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+year=Input_date.year
+month=Input_date.month
+day=Input_date.day
+#A value which shows the weekDay 
+Datevalue=calendar.weekday(year,month,day)
+WeekDay_result=Weekdays[Datevalue]
+srt.write("The Week Day is:",WeekDay_result)
